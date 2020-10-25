1608d5a0ff3376e072f34c1b6c9497ccab8625d0
akila2024 <73436371+akila2024@users.noreply.github.com>
2020-10-25 23:08:07 +0530

    Add files via upload

---
+++ positivenos.py
@@ -0,0 +1,7 @@
+list1=[12,-7,5,64,-14]
+print("Original numbers in the list:",list1)
+new_list1=list(filter(lambda x: x>0,list1))
+print("Positive numbers in the list1 : ",new_list1)
+
+
+
---
+++ positivenos1.py
@@ -0,0 +1,7 @@
+list2=[12,14,-95,3]
+print("Original numbers in the list:",list2)
+new_list2=list(filter(lambda x: x>0,list2))
+print("Positive numbers in the list1 : ",new_list2)
+
+
+
